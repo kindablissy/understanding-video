487886f4fc8249f795ad2826decd7cfa17c16a0b
Blissy <restricted.karki@gmail.com>
2025-01-12 19:08:11 +0545

    idk got stuck and finally found docs.

---
+++ ReadME.md
@@ -0,0 +1,15 @@
+# MP4 Study
+A simple code to hopefully render fome mp4 frames.
+Below is just the doc of what i've got so far :)
+
+## Basics
+    * All the data are in format \[4bytes length, 4 bytes data type, {length} bytes data content] (mostly);
+    * These "things" are called box or atoms.
+    * mp4 files have (mostly) ftyp(filetype) data type. it again follows the same format above.
+    * a box can contain other boxes within it. (apparently called container box. who cares)
+    * main boxes that is useful are mdat box and moov box. 
+### Box types Reference
+    ![boxtypes part1](./boxtypes1.png)
+    ![boxtypes part2](./boxtypes2.png)
+    ![boxtypes part3](./boxtypes3.png)
+
--- mp4_parser.c
+++ mp4_parser.c
@@ -16,12 +16,26 @@ typedef struct mp4_file_header {
   char *technologies_used;
 } mp4_file_header;
 
+
+/* 
+ * TODO: 
+ * This will be super slow ::::
+ * Try to load a larger chunk of data at once and then do the parsing stuff;
+*/
 int parse_mp4(const char *filename) {
   FILE *mp4file = fopen(filename, "rb");
+  unsigned long file_size = 0;
+
+  fseek(mp4file, 0L, SEEK_END);
+  file_size = ftell(mp4file);
+  fseek(mp4file, 0L, 0);
+
+  printf("filesize %ld\n", file_size);
 
   mp4_file_header header;
 
-  fread(&header, sizeof(header), 1, mp4file);
+  printf("current file position:: %ld\n", ftell(mp4file));
+  fread(&header, sizeof(header) - sizeof(header.technologies_used), 1, mp4file);
 
   printf("offset:              %u\n", __bswap_constant_32(header.offset));
   printf("ftype:               %c%c%c%c\n", header.ftypestring[0],
@@ -30,11 +44,64 @@ int parse_mp4(const char *filename) {
          header.major_brand[1], header.major_brand[2], header.major_brand[3]);
   printf("major brand version: %u\n", __bswap_constant_32(header.m_brand_version));
 
-  header.technologies_used = (char *)malloc(header.offset - 16);
-  fread(header.technologies_used, 8, header.offset - 16, mp4file);
+  printf("current file position:: %ld\n", ftell(mp4file));
+  header.technologies_used = (char *)malloc(header.offset - ftell(mp4file));
+  fread(header.technologies_used, sizeof(char) * 16, 1, mp4file);
+  printf("current file position:: %ld\n", ftell(mp4file));
   printf("technologies used string: %s\n", header.technologies_used);
 
+  // Time to read some data
+  uint32_t box_size;
+  char box_name[4];
+  printf("current file position:: %ld\n", ftell(mp4file));
+  
+  fread(&box_size, sizeof(box_size), 1, mp4file);
+  printf("databox size: %u\n", __bswap_constant_32(box_size));
+  fread(box_name, sizeof(box_name), 1, mp4file);
+  printf("databox name:: %s\n", box_name); /* free */
+
+
+
+  /* 
+   * TODO::: do something abstract
+   * just doing this because of the sample file i'm using and the previous box was "free" box
+  */
+
+  printf("reading another block: \n");
+  fread(&box_size, sizeof(box_size), 1, mp4file);
+  box_size = __bswap_constant_32(box_size);
+  printf("databox size: %u\n", box_size);
+  fread(box_name, sizeof(box_name), 1, mp4file);
+  printf("databox name:: %s\n", box_name); /* mdat box */
+
+  printf("reading another block: \n");
+  char *data = (char *) malloc(box_size - ftell(mp4file));
+  if (!data) {
+    printf("no mem??");
+    return 1;
+  }
+  fread(data, (box_size - 8), 1, mp4file);
+  printf("\n\nreading main data block: \n");
+  /* since the file didnt contain chars "wide" */
+  box_size = __bswap_constant_32(((uint32_t *) data)[0]);
+  printf("databox size: %u\n", box_size);
+
+
+  printf("current file position:: %ld\n", ftell(mp4file));
+
   fclose(mp4file);
+  return 0;
+}
+
+int parse_mdat(char *buffer) {
+  uint32_t box_size = __bswap_constant_32(((uint32_t *) buffer)[0]);
+  unsigned long offset = 8;
+
+  
+  return 0;
+}
+
+int parse_nal_unit(char *buffer) {
   return 0;
 }
 
